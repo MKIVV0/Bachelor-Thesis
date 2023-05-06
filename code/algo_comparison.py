f53f65c67d1eae903d92c6912ca406ca808f8ace
Matt <huangzhangying@live.it>
2023-05-06 11:02:03 +0200

    plotting percentage deviation histos fixed

--- code/algo_comparison.py
+++ code/algo_comparison.py
@@ -96,9 +96,9 @@ def plot_histogram(data, title):
     # fitting curve function
     popt, pcov = curve_fit(f=gaussian, xdata=bin_centers, ydata=hist, p0=[max(hist), local_mean, local_std])
     print(popt, '\n', pcov)
-
+ 
     fig, ax = plt.subplots(1, 1)
-    ax.set_title("{}:\nμ: {:.2f}\nσ: {:.2f}".format(
+    ax.set_title("{}:\nμ: {:.5e}\nσ: {:.5e}".format(
         title, local_mean, local_std))
     # plots the fitting line curve
     ax.plot(bin_centers, gaussian(bin_centers, *popt), 'r', label='gaussian fit')
@@ -148,7 +148,7 @@ def plot_epsmedians(x, epsilon_median_lists, epsilonlist, k, mu, sigma, sigma_co
 # The object parameter is only used when a two-heaps algorithm is passed
 # The median_function parameter is used when only a function is needed for estimating the median
 # it returns the i-th value for each algorithm run
-
+'''
 def get_ith_iteration(mean, std, num_of_iteration, median_function1, median_function2, epsilon):
     x_i = 0
     tmp_trial_median1 = 0
@@ -170,8 +170,33 @@ def get_ith_iteration(mean, std, num_of_iteration, median_function1, median_func
             break
 
     return float(tmp_trial_median1), float(tmp_trial_median2), float(tmp_trial_median3)
+'''
+
+def get_ith_iteration(mean: float, std: float, num_of_iteration: int, median_function1: float, median_function2: float, epsilon_values: list[float]):
+    x_i = 0
+    tmp_trial_median1: float = 0
+    tmp_trial_median2: list[float] = [0.0 for i in range(len(epsilon_values))]
+    tmp_trial_median3: float = 0
+    tmp_data = []
+    tmp_two_heaps = th.TwoHeaps()
+    for i in range(1000):
+        x_i = rnd.normalvariate(mean, std)
+        tmp_data.append(x_i)
+        tmp_two_heaps.insert(x_i)
+        # The epsilon-median is here because it need past values, which
+        # are not stored to calculate the current median
+        for j in range(len(epsilon_values)):
+            tmp_trial_median2[j] = median_function2(
+                x_i, epsilon_values[j], i, tmp_trial_median2[j])  # epsilon-median
+        if i == num_of_iteration:
+            tmp_trial_median1 = median_function1(tmp_data)  # numpy.median
+            tmp_trial_median3 = tmp_two_heaps.findMedian()
+            break
+
+    return float(tmp_trial_median1), list(tmp_trial_median2), float(tmp_trial_median3)
 
 
+'''
 def plot_error_histograms(mean, std, epsilon):
     num_of_trials = 1000
     ith_values1 = []  # error values for numpy.median
@@ -214,8 +239,61 @@ def plot_error_histograms(mean, std, epsilon):
         ax.set_title("mean: {:e}\nstd: {:e}".format(
             np.mean(ith_values3), np.std(ith_values3)))
         ax.hist(ith_values3, color='darkblue')
+'''
 
 
+def plot_error_histograms(mean, std, epsilon_values: list[float]):
+    num_of_trials = 1000
+    ith_values1: list[float] = []  # error values for numpy.median
+    ith_values2: list[list[float]] = [[] for i in epsilon_values]  # error values for ε-median1
+    ith_values3: list[float] = []  # error values for two-heaps median
+    # Executes num_of_trials trials and gets the i-th value for each trial
+    # The other values are thrown away
+    print("LIST", ith_values2)
+    for i in range(num_of_trials):
+        x, y, z = get_ith_iteration(
+            mean, std, 800, np.median, epsilon_median, epsilon_values)
+        ith_values1.append(x)
+        for index in range(len(ith_values2)):
+            ith_values2[index].append(y[index])
+        ith_values3.append(z)
+
+    #print("VALUES\n")
+    #for a in ith_values2:
+        #print(a[:4])
+    #print(ith_values2[0:10])
+    #print(ith_values3[0:10])
+
+    def to_percent_format(x): return (x/mean) - 1
+    ith_values1 = list(map(to_percent_format, ith_values1))
+    for index in range(len(ith_values2)):
+        ith_values2[index] = list(map(to_percent_format, ith_values2[index]))
+    ith_values3 = list(map(to_percent_format, ith_values3))
+
+    num_of_graphs = 1 + len(ith_values2) + 1
+
+    plot_histogram(ith_values1, "Numpy.median error histogram")
+    
+    for index in range(len(ith_values2)):
+        plot_histogram(ith_values2[index], "Epsilon-median percentage deviations histogram, ε = {}".format(epsilon_values[index]))
+
+    plot_histogram(ith_values3, "Two-heaps median error histogram")
+    '''
+    for ax in ax_i:
+        ax.title.set_text("Numpy.median error histogram")
+        ax.set_title("mean: {:e}\nstd: {:e}".format(
+            np.mean(ith_values1), np.std(ith_values1)))
+        ax.hist(ith_values1, color='darkblue')
+
+        ax.title.set_text("Epsilon-median error histogram")
+        ax.set_title("mean: {:e}\nstd: {:e}".format(
+            np.mean(ith_values2), np.std(ith_values2)))
+        ax.hist(ith_values2, color='darkblue')
+
+        ax.title.set_text("Two-heaps median error histogram")
+        ax.set_title("mean: {:e}\nstd: {:e}".format(
+            np.mean(ith_values3), np.std(ith_values3)))
+        ax.hist(ith_values3, color='darkblue') '''
 
 # MAIN FUNCTION
 # This main executes: the three algorithm confrontation, histogram plotting with gaussian-fitting,
@@ -304,8 +382,9 @@ def main():
         
 
     # This section represents the error histogram plot
-    for eps in generated_epsilons:
-        plot_error_histograms(mean, std, eps)
+    #for eps in generated_epsilons:
+        #plot_error_histograms(mean, std, eps)
+    plot_error_histograms(mean, std, generated_epsilons)
     plot_median_estimations(x, numpy_medians, generated_epsilon_medians[7], two_heaps_medians, mean, std, generated_epsilons[7])
     # plot_histogram(mean, std, generated_nums, 25, 3, data_size)
     # histogram(generated_nums, 25, mean, std)
