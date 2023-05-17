f5f26afab508e86d095bc9cb84ba445046e5a4d9
Matt <huangzhangying@live.it>
2023-05-17 21:25:55 +0200

    added k values in plots and removed get indexes part

--- code/percentage_deviations.py
+++ code/percentage_deviations.py
@@ -4,19 +4,6 @@ import TwoHeaps as th
 import random as rnd
 from matplotlib.pyplot import show as plt_show
 
-
-''' 
-We want to get, for each trial, the index of the element that, given a trend-plot, goes below 1% and 0.1%.
-This method is integrated into get_ith_iteration.
-'''
-def get_indexes(mu, x_i, i):
-    cond1 = 1 #1%
-    cond2 = 0.1 #0.1%
-    if ((x_i/mu) -1) * 100 <= 1:
-        write_to_file(i, "1percent")
-    if ((x_i/mu) - 1) * 100  <= 0.1:
-        write_to_file(i, "1permille")
-
 # Represents ONE median-estimation trial
 # The object parameter is only used when a two-heaps algorithm is passed
 # The median_function parameter is used when only a function is needed for estimating the median
@@ -28,6 +15,7 @@ def get_ith_iteration(mu: float, sigma: float, num_of_iteration: int, median_fun
     tmp_trial_median3: float = 0
     tmp_data = []
     tmp_two_heaps = th.TwoHeaps()
+
     for i in range(1000):
         x_i = rnd.normalvariate(mu, sigma)
         tmp_data.append(x_i)
@@ -37,6 +25,7 @@ def get_ith_iteration(mu: float, sigma: float, num_of_iteration: int, median_fun
         for j in range(len(epsilon_values)):
             tmp_trial_median2[j] = median_function2(
                 x_i, epsilon_values[j], i, tmp_trial_median2[j])  # epsilon-median
+
         if i == num_of_iteration:
             tmp_trial_median1 = median_function1(tmp_data)  # numpy.median
             tmp_trial_median3 = tmp_two_heaps.findMedian()
@@ -46,7 +35,7 @@ def get_ith_iteration(mu: float, sigma: float, num_of_iteration: int, median_fun
 
 
 # The parameters shown in each plots are locally calculated and are not the known ones
-def plot_error_histograms(mu, sigma, epsilon_values: list[float]):
+def plot_error_histograms(mu, sigma, epsilon_values: list[float], k_values: list[float]):
     num_of_trials = 1000
     ith_values1: list[float] = []  # error values for numpy.median
     ith_values2: list[list[float]] = [[] for i in epsilon_values]  # error values for ε-median1
@@ -61,6 +50,7 @@ def plot_error_histograms(mu, sigma, epsilon_values: list[float]):
             ith_values2[index].append(y[index])
         ith_values3.append(z)
 
+    # local mus and sigmas
     mu1 = mean(ith_values1)
     mu2 = [mean(ith_values2[i]) for i in range(len(ith_values2))]
     mu3 = mean(ith_values3)
@@ -76,16 +66,29 @@ def plot_error_histograms(mu, sigma, epsilon_values: list[float]):
 
 
     plot_histogram(ith_values1, "Numpy.median error histogram", mu1, sigma1, format='percent')
-    for index in range(len(ith_values2)): plot_histogram(ith_values2[index], "Epsilon-median percentage deviations histogram, ε = {:.2f}".format(epsilon_values[index]), mu2[index], sigma2[index], format='percent')
+    for index in range(len(ith_values2)): plot_histogram(ith_values2[index], "Epsilon-median percentage deviations histogram, k = {:.0f}, ε = {:.2f}".format(k_values[index], epsilon_values[index]), mu2[index], sigma2[index], format='percent')
     plot_histogram(ith_values3, "Two-heaps median error histogram", mu3, sigma3, format='percent')
+
+
+# This method is needed for deserilalizing the values from the file generated_epsilons.txt
+def split_k_epsilons(list_of_vals: list[str]):
+    k_values = []
+    epsilons = []
+    for i in list_of_vals:
+        tmp1, tmp2 = i.split('#')
+        k_values.append(float(tmp1))
+        epsilons.append(float(tmp2))
+    
+    return k_values, epsilons
     
 # MAIN ENTRY
 def main():
+
     # parameters
-    mean = pow(10, 5)
-    std = sqrt(mean)
-    generated_epsilons = read_from_file()
-    plot_error_histograms(mean, std, generated_epsilons)
+    mu = pow(10, 5)
+    sigma = sqrt(mu)
+    k_values, generated_epsilons = split_k_epsilons(read_from_file())
+    plot_error_histograms(mu, sigma, k_values, generated_epsilons)
     plt_show()
 
 if __name__ == "__main__":
