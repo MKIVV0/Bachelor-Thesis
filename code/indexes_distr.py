f423e261553a88af7b4e4427ccfb1f0ae3ad9244
Matt <huangzhangying@live.it>
2023-05-18 17:31:26 +0200

    implemented 1000 story cycle and k_bar plots (to revise)

--- code/indexes_distr.py
+++ code/indexes_distr.py
@@ -1,4 +1,4 @@
-from numpy import sqrt
+from numpy import sqrt, mean, histogram
 import matplotlib.pyplot as plt
 import random as rnd
 from misc import read_from_file, epsilon_median
@@ -23,9 +23,9 @@ def check_index(mu, x_i):
     cond2 = False #0.1%
     #NB: the absolute value is important, otherwise, given a normal distributed graph, all the values from one side will be taken
     #Another reminder: we are setting two std limiters, so this is another reason for which abs is important
-    if abs( ((x_i/mu) - 1) * 100) < 1: 
+    if abs( ((x_i/mu) - 1) * 100) < 0.1: 
         cond1 = True
-    if abs( ((x_i/mu) - 1) * 100)  < 0.1:
+    if abs( ((x_i/mu) - 1) * 100)  < 0.02:
         cond2 = True
 
     return cond1, cond2
@@ -42,6 +42,26 @@ def plot_data_distribution(xvalues, title):
     ax.legend()
 
 
+def plot_k_barplots(k_values, y_values1, y_values2):    # TO REVISE
+
+    iter_means1 = [mean(sublist) for sublist in y_values1]
+    iter_means2 = [mean(sublist) for sublist in y_values2]
+
+    fig, ax = plt.subplots(2, 1)
+
+
+    ax[0].set_title("iterations which value go under 0.1%")
+    #ax[0].plot(k_values, iter_means1)
+    ax[0].hist(iter_means1)
+    ax[0].set_ylabel("Iteration mean values")
+
+    ax[1].set_title("iterations which value go under 0.02%")
+    #ax[1].plot(k_values, iter_means2)
+    ax[1].hist(iter_means2)
+    ax[1].set_xlabel("K constants")
+    ax[1].set_ylabel("Iteration mean values")
+
+
 # MAIN ENTRY
 def main():
 
@@ -56,20 +76,23 @@ def main():
     one_percent = [[] for i in range(len(k_values))]
     one_permille = [[] for i in range(len(k_values))] 
 
-    # simulates a stream of 1000 data (1 story)
-    for i in range(1000):
-        datapoint = rnd.normalvariate(mu, sigma)
-            
-        for j in range(len(k_values)):
-            actual_medians[j] = epsilon_median(datapoint, epsilons[j], i, actual_medians[j])
-            cond1, cond2 = check_index(mu, actual_medians[j])
-            if cond1 == True: one_percent[j].append(i)
-            if cond2 == True: one_permille[j].append(i)
+    for stories in range(1000):
+        # simulates a stream of 1000 data (1 story)
+        for i in range(1000):
+            datapoint = rnd.normalvariate(mu, sigma)
+                
+            for j in range(len(k_values)):
+                actual_medians[j] = epsilon_median(datapoint, epsilons[j], i, actual_medians[j])
+                cond1, cond2 = check_index(mu, actual_medians[j])
+                if cond1 == True: one_percent[j].append(i)
+                if cond2 == True: one_permille[j].append(i)
     
 
     for i in range(len(k_values)):
-        plot_data_distribution(one_percent[i], 'Iterations which numbers go below 1% for k = {:.0f}, data count = {}'.format(k_values[i], len(one_percent[i])))
-        #plot_data_distribution(one_permille[i], 'Iterations which numbers go below 0.1% for k = {:.0f}, data count = {}'.format(k_values[i], len(one_permille[i])))
+        plot_data_distribution(one_percent[i], 'Iterations which numbers go below 0.1% for k = {:.0f}, data count = {}'.format(k_values[i], len(one_percent[i])))
+        plot_data_distribution(one_permille[i], 'Iterations which numbers go below 0.02% for k = {:.0f}, data count = {}'.format(k_values[i], len(one_permille[i])))
+
+    plot_k_barplots(k_values, one_percent, one_permille)
 
     plt.show()
 
